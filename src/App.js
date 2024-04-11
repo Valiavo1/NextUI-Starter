cacd3abce38d5938c30ffc424d6f9d2303239cb4
Valiavo1 <valiavoandriantsoa30@gmail.com>
2024-04-11 14:54:08 +0300

    next ui done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import {Card, CardBody} from "@nextui-org/react";
+import Tabletest from "./Tabletest";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
                 <p>Make beautiful websites regardless of your design experience.</p>
             </CardBody>
         </Card>
+        <Tabletest/>
       <h1 className="text-red-400">APP</h1>
     </div>
   );
